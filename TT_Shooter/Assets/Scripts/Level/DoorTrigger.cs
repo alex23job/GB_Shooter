a5f4adea057ed413355c7ad4a18f1dfb2853baec
alex23job <106354657+alex23job@users.noreply.github.com>
2025-10-24 21:00:50 +0300

    Adding door open

---
+++ TT_Shooter/Assets/Scripts/Level/DoorTrigger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+    [SerializeField] private LevelControl levelControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (levelControl != null)
+            {
+                if (levelControl.IsKey)
+                {
+                    if (door != null)
+                    {
+                        door.GetComponent<ExitLevel>().DoorOpen();
+                    }
+                }
+            }
+        }
+    }
+}
--- TT_Shooter/Assets/Scripts/Level/ExitLevel.cs
+++ TT_Shooter/Assets/Scripts/Level/ExitLevel.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class ExitLevel : MonoBehaviour
 {
     [SerializeField] private LevelControl levelControl;
+    [SerializeField] private bool isDoorBody = false;
+    [SerializeField] private float speedUP = 10f;
+
+    private bool isOpen = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +18,13 @@ public class ExitLevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isOpen && isDoorBody)
+        {
+            Vector3 pos = transform.position;
+            pos.y += speedUP * Time.deltaTime;
+            transform.position = pos;
+            if (pos.y > 4.9f) isOpen = false;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -23,5 +33,10 @@ public class ExitLevel : MonoBehaviour
         {
             if ((levelControl.IsKey) && (other.CompareTag("Player"))) levelControl.ViewEndPanel(); 
         }
+    }
+
+    public void DoorOpen()
+    {
+        isOpen = true;
     }
 }
